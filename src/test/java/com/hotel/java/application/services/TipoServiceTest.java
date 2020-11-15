2e3e49715752c0aab1c24a079e6077a5b3f90d7a
hiitoos <adrianhitos@gmail.com>
2020-11-15 18:48:48 +0100

    TipoServiceTest added

--- src/test/java/com/hotel/java/application/services/TipoServiceTest.java
+++ src/test/java/com/hotel/java/application/services/TipoServiceTest.java
@@ -4,6 +4,7 @@ import com.hotel.java.JavaApplication;
 import com.hotel.java.application.models.TipoModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -42,5 +43,16 @@ public class TipoServiceTest {
 
         when(tipoService.showAllTipos ()).thenReturn (tipos);
         assertThat(tipoService.showAllTipos ()).isEqualTo (tipos);
+    }
+
+    //This test will pass if we have data on BBDD
+
+    @Autowired
+    private TipoService tipos;
+
+    @Test
+    @DisplayName ("Devuelve una lista de tipos")
+    public void ShouldReturnAnArray(){
+        assertThat (tipos.showAllTipos ()).hasOnlyElementsOfType (TipoModel.class);
     }
 }
